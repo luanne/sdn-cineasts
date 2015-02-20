78680e05e2c4dd941238e152f52379e490d5dea5
Luanne Misquitta <luanne.coutinho@gmail.com>
2015-02-20 10:11:39 +0530

    Added the web stuff, doesn't work yet

--- graphaware/cineasts/src/main/java/org/neo4j/cineasts/repository/MovieRepository.java
+++ graphaware/cineasts/src/main/java/org/neo4j/cineasts/repository/MovieRepository.java
@@ -1,8 +1,11 @@
 package org.neo4j.cineasts.repository;
 
 import org.neo4j.cineasts.domain.Movie;
+import org.neo4j.cineasts.domain.User;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+
+import java.util.List;
 
 /**
  * @author mh
@@ -18,12 +21,10 @@ public interface MovieRepository extends GraphRepository<Movie> {
     @Query("MATCH (movie:Movie) WHERE movie.title =~ '.*{0}.*' RETURN movie")
     Iterable<Movie> findByTitleLike(String title);
 
-  /*  @Query( "start user=node({0}) " +
-                    " match user-[r:RATED]->movie<-[r2:RATED]-other-[r3:RATED]->otherMovie " +
-                    " where r.stars >= 3 and r2.stars >= r.stars and r3.stars >= r.stars " +
-                    " return otherMovie, avg(r3.stars) as rating, count(*) as cnt" +
+    @Query( "match (user:User)-[r:RATED]->(movie)<-[r2:RATED]-(other)-[r3:RATED]->(otherMovie) " +
+                    " where ID(user)={0} and r.stars >= 3 and r2.stars >= r.stars and r3.stars >= r.stars " +
+                    " with otherMovie, avg(r3.stars) as rating, count(*) as cnt" +
                     " order by rating desc, cnt desc" +
-                    " limit 10" )
-    List<MovieRecommendation> getRecommendations(User user);
-        */
+                    " return otherMovie limit 10" )
+    List<Movie> getRecommendations(User user);
 }
